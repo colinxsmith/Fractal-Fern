13cc7e303acead9550f1a68aa51a9a8e8209288e
Colin Smith <colinxsmith@gmail.com>
2017-06-04 17:46:16 +0100

    Update fern.js

--- fern.js
+++ fern.js
@@ -1,4 +1,4 @@
-function fern(canv, ww, hh, a, b, c, d) {
+function fern(canv, ww, hh, a, b, c, d) {//Data from page 87 of "Fractals Everywhere"
     var a = [0, .85, .2, -.15];
     var b = [0, .04, -.26, .28];
     var c = [0, -.04, .23, .26];
@@ -8,14 +8,14 @@ function fern(canv, ww, hh, a, b, c, d) {
     var p = [];
     var cp = [];
     var maxn = 200000, i;
-    var psum = 0;
+    var psum = 0;//Here we calulate the probabilities from matrix determinants
     for (i = 0; i < 4; ++i) {
         p[i] = Math.abs(a[i] * d[i] - b[i] * c[i]);
         psum += p[i];
     }
     for (i = 0; i < 4; ++i) {
         p[i] /= psum;
-        if (p[i] < 1e-2)
+        if (p[i] < 1e-2)//If a probabilty is very small the picture isn't so good so just use 1/100
             p[i] = 1e-2;
     }
     cp[0] = 0;
@@ -27,7 +27,7 @@ function fern(canv, ww, hh, a, b, c, d) {
             cp[i] /= cp[4];
         }
     }
-    var j = 0;
+    var j = 0;//Random Iteration Algorithm
     var x = 0;
     var y = 0;
     var xn = 0;
